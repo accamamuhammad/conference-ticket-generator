68a149b602c816fd98ca07dca9d87f764d7049db
accama <accamamuhammad@gmail.com>
2024-12-24 16:28:24 +0100

    App.jsx setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import spiralElementBottom from "../public/assets/images/pattern-squiggly-line-bottom.svg";
+import spiralElementTop from "../public/assets/images/pattern-squiggly-line-top.svg";
 
 function App() {
-  return <></>;
+  return (
+    <main className="main-bg">
+      {/* Background elements */}
+      <img
+        src={spiralElementBottom}
+        alt="spiralElementBottom"
+        className="spiral-element-bottom"
+      />
+      <img
+        src={spiralElementTop}
+        alt="spiralElementTop"
+        className="spiral-element-top"
+      />
+      {/* Main element */}
+    </main>
+  );
 }
 
 export default App;
