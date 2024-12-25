b8238fa9e1c15c26e5388c5cd04eb25cd7f52f63
accama <accamamuhammad@gmail.com>
2024-12-25 12:33:25 +0100

    get data from database

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import LogoOutline from "../../public/assets/images/logo-mark.svg";
+import app from "../config/firebase";
+import { getDatabase, get, ref } from "firebase/database";
 
 const Ticket = () => {
+  const [data, setData] = useState([]);
+
+  const getDataFromDatabase = async () => {
+    const db = getDatabase(app);
+    const ticketRef = ref(db, "tickets");
+    try {
+      const snapshot = await get(ticketRef);
+      if (snapshot.exists()) {
+        const data = Object.values(snapshot.val());
+        setData(data);
+        console.log(data);
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    // getDataFromDatabase();
+  }, []);
+
   return (
     <div className="form-container">
       {/* Logo Header */}
@@ -19,7 +43,7 @@ const Ticket = () => {
         <span>jonatan@email.com</span> and will send updates in <br /> the run
         up to the event
       </p>
-      <div class="circle-cut"></div>
+      <div className="circle-cut"></div>
     </div>
   );
 };
